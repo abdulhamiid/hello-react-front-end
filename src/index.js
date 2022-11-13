5b989e76d0446e190b05eb0a2bb5513d6312eadb
abdulhamiid <adioabdulhamid@yahoo.com>
2022-11-13 13:06:30 +0100

    Add page routes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './components/redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+          <App />
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
