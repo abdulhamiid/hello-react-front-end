271c94283ab5f0f256b213ed85ca46dd19ae6aaa
abdulhamiid <adioabdulhamid@yahoo.com>
2022-11-16 21:06:34 +0100

    Align text to the center

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   return (
-    <div>
+    <div className="container">
       <h1>Hello World!</h1>
       <Link to='/greetings' exact="true">Greetings</Link>
     </div>
